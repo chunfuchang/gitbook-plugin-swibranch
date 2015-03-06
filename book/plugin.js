d8900ce6c07d0c112c8c7923dca072e9df2ba310
chunfuchang <chunfuchang@potix.com>
2015-03-06 16:08:50 +0800

    fix page.change not working

--- book/plugin.js
+++ book/plugin.js
@@ -1,20 +1,21 @@
-require(["gitbook"], function(gitbook) {
-    gitbook.events.bind("page.change", function() {
-        // do something
-    });
+var paths = window.location.pathname.split('/');
+var host = window.location.host;
+var select = $('#branches');
 
-    gitbook.events.bind("start", function() {
-        var select = $('#branches');
-        //select.prop('selectedIndex', -1);
-        select.change(function () {
-            var paths = window.location.pathname.split('/');
-            paths[2] = $(this).val(); //designate branch
-            var link = '';
-            paths.forEach(function(path) {
-                if (path.length > 0)
-                    link += '/' + path;
-            });
-            window.location.href = window.location.protocol + '//' + window.location.host + link;
-        });
-    });
+var changeBranch = function() {
+	select.val(paths[2]);
+	select.change(function () {
+		paths[2] = $(this).val(); //designate branch
+		var link = '';
+		paths.forEach(function(path) {
+			if (path.length > 0)
+				link += '/' + path;
+		});
+		window.location.href = window.location.protocol + '//' + host + link;
+	});
+}
+require(["gitbook"], function(gitbook) {
+	gitbook.events.bind("page.change", changeBranch);
+
+	gitbook.events.bind("start", changeBranch);
 });
