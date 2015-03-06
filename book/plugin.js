270a3510ace9f00d1964cf36bf8f5024bb39aed4
chunfuchang <chunfuchang@potix.com>
2015-03-06 08:33:51 +0000

    really fix page.change bug...hopefully...

--- book/plugin.js
+++ book/plugin.js
@@ -1,20 +1,19 @@
-var paths = window.location.pathname.split('/');
-var host = window.location.host;
-var select = $('#branches');
-
-var changeBranch = function() {
-	select.val(paths[2]);
-	select.change(function () {
-		paths[2] = $(this).val(); //designate branch
-		var link = '';
-		paths.forEach(function(path) {
-			if (path.length > 0)
-				link += '/' + path;
-		});
-		window.location.href = window.location.protocol + '//' + host + link;
-	});
-}
 require(["gitbook"], function(gitbook) {
+	var changeBranch = function() {
+		var paths = window.location.pathname.split('/');
+		var host = window.location.host;
+		var select = $('#branches');
+		select.val(paths[2]);
+		select.change(function () {
+			paths[2] = $(this).val(); //designate branch
+			var link = '';
+			paths.forEach(function(path) {
+				if (path.length > 0)
+					link += '/' + path;
+			});
+			window.location.href = window.location.protocol + '//' + host + link;
+		});
+	}
 	gitbook.events.bind("page.change", changeBranch);
 
 	gitbook.events.bind("start", changeBranch);
